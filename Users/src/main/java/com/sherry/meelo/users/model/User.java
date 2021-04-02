868dfd6f4550fa2cfdb60954e02522d2f797649a
Sehaj <iamsherrysingh@gmail.com>
2021-04-02 01:07:17 -0400

    Added token to User backend

--- Users/src/main/java/com/sherry/meelo/users/model/User.java
+++ Users/src/main/java/com/sherry/meelo/users/model/User.java
@@ -58,8 +58,7 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [userName=" + userName + ", name=" + name + ", password=" + password + ", address=" + address
-				+ "]";
+				+ ", token=" + token + "]";
 	}
-	
-	
+
 }
